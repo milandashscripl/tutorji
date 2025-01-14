bd42c24e6ecec3536f02caadbad3db6de3fdbca4
milandashscripl <immilankumar01@gmail.com>
2025-01-14 21:00:34 +0530

    profile pic setup

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,54 +26,6 @@ exports.registerUser = async (req, res) => {
 };
 
 
-// exports.loginUser = async (req, res) => {
-//     try {
-//       const { email, password } = req.body;
-//       const user = await User.findOne({ email });
-  
-//       if (!user || !(await bcrypt.compare(password, user.password))) {
-//         return res.status(401).json({ message: "Invalid credentials" });
-//       }
-  
-//       const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-  
-//       res.json({
-//         token,
-//         user: {
-//           id: user._id,
-//           name: user.name,
-//           email: user.email,
-//           contact: user.contact,
-//           aadhar: user.aadhar,
-//           address: user.address,
-//           profilePicture: user.profilePicture,
-//         },
-//       });
-//     } catch (err) {
-//       res.status(500).json({ error: err.message });
-//     }
-//   };
-
-
-
-
-// Login User
-// exports.loginUser = async (req, res) => {
-//     try {
-//         const { email, password } = req.body;
-//         const user = await User.findOne({ email });
-
-//         if (!user || !(await bcrypt.compare(password, user.password))) {
-//             return res.status(401).json({ message: "Invalid credentials" });
-//         }
-
-//         const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-//         res.json({ token, userId: user._id });
-//     } catch (err) {
-//         res.status(500).json({ error: err.message });
-//     }
-// };
-
 
 
 
@@ -82,13 +34,15 @@ exports.loginUser = async (req, res) => {
     try {
         const { email, password } = req.body;
         const user = await User.findOne({ email });
-
+        profilePictureUrl = user.profilePicture
+        ? `https://tutroji.onrender.com/uploads/${user.profilePicture}`
+         : null;
         if (!user || !(await bcrypt.compare(password, user.password))) {
             return res.status(401).json({ message: "Invalid credentials" });
         }
 
         const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
+        
         res.json({
             token,
             user: {
@@ -98,7 +52,7 @@ exports.loginUser = async (req, res) => {
                 contact: user.contact,
                 aadhar: user.aadhar,
                 address: user.address,
-                profilePicture: `https://tutroji.onrender.com/${user.profilePicture}`,
+                profilePicture: user.profilePicture,
             },
             userId: user._id, // Include userId for backward compatibility
         });
