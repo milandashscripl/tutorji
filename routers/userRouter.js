36134c8ff407b023fa4dbbe478d1dc7a39673dbb
milandashscripl <immilankumar01@gmail.com>
2025-01-24 14:33:08 +0530

    update profile

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -27,7 +27,7 @@ const express = require('express');
 const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const cloudinary = require('../config/cloudinaryConfig');
-const { registerUser, loginUser, getUserProfile } = require('../controllers/userController');
+const { registerUser, loginUser, getUserProfile, updateUser } = require('../controllers/userController');
 const router = express.Router();
 
 // Configure Cloudinary storage with multer
@@ -45,5 +45,7 @@ const upload = multer({ storage });
 router.post('/register', upload.single('profilePicture'), registerUser);
 router.post('/login', loginUser);
 router.get('/profile/:id', getUserProfile);
+router.patch('/update/:id', upload.single('profilePicture'), updateUser);
+
 
 module.exports = router;
