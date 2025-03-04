a95ac27fd50fe267b5f3fc3c89a2603b7932a99a
milandashscripl <immilankumar01@gmail.com>
2025-03-04 11:45:32 +0530

    added chatting

--- routers/querryRoutes.js
+++ routers/querryRoutes.js
@@ -2,14 +2,14 @@ const express = require("express");
 const router = express.Router();
 const { createQuery, getAllQueries, getUserQueries } = require("../controllers/querryController");
 const { protect, admin } = require("../middleware/authMiddleware"); 
-
+   
 // 📝 User submits a query
 router.post("/submit", protect, createQuery);
 
 // 📜 Admin fetches all queries
 router.get("/all", protect, admin, getAllQueries);
 
-// 👤 User fetches their own queries
+// 👤 User fetches their own queries ..
 router.get("/my-queries", protect, getUserQueries);
 
 module.exports = router;
