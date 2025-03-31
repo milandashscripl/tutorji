66f32eb7640669c3ce397c414035e28330159d50
milandashscripl <immilankumar01@gmail.com>
2025-03-31 11:08:52 +0530

    new

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: 'https://tutorji.netlify.app'
+    origin: 'https://libpathasala.netlify.app'
   }));  
 app.use(bodyParser.json());
 app.use('/uploads', express.static('uploads'));
